cb92d8189d4b833a5c19feb11b7882ef7d5a331d
Esther <estherfinsen@gmail.com>
2024-02-05 09:53:04 +0100

    hej

--- arrays/js.js
+++ arrays/js.js
@@ -1,5 +1,23 @@
+/*
 let letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
 console.log(letters[4]);
 let newarr = letters;
 newarr[4] = "*";
 console.log(letters[4]);
+*/
+
+let people = ["Harry", "Ron", "Hermione"];
+let result;
+result = people.push("Draco");
+result = people.pop();
+result = people.push("Neville");
+result = people.push("Luna");
+result = people.slice(0, 3);
+result = people.splice(1, 0, "Cho");
+people[1] = "Ginny";
+result = people.push("Fred", "George");
+result = people.indexOf("Fred");
+result = people.splice(result, 1);
+
+console.log(result);
+console.log(people);
