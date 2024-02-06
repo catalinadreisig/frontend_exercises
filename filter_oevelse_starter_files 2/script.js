48fabc9cfb88260804fb809320efd7b40433bae1
Esther <estherfinsen@gmail.com>
2024-02-06 11:12:41 +0100

    jhgb

--- filter_oevelse_starter_files 2/script.js
+++ filter_oevelse_starter_files 2/script.js
@@ -1,38 +1,5 @@
 /*
-const vehicles = [
-  { type: "Bus", fuel: "Diesel", passengers: 45, stops: ["Nørrebrogade", "Elmegade"] },
-  { type: "Bil", fuel: "Benzin", passengers: 4, ownedBy: "Klaus" },
-  { type: "Cykel", fuel: "Rugbrød", passengers: 0, ownedBy: "Jonas", isElectric: true },
-  { type: "Bil", passengers: 5, ownedBy: "Elon", isElectric: true },
-  { type: "MC", fuel: "Benzin", passengers: 2, ownedBy: "Fonda" },
-  { type: "Cykel", fuel: "Rugbrød", passengers: 2, ownedBy: "Vingegård", isTandem: true },
-  { type: "MC", fuel: "Benzin", passengers: 2, ownedBy: "Yolanda" },
-  { type: "Knallert", fuel: "Benzin", passengers: 1, ownedBy: "Børge" },
-  { type: "Knallert", fuel: "Benzin", passengers: 1, ownedBy: "Jonas" },
-  { type: "Løbehjul", passengers: 1, isElectric: true },
-];
 
-const ulPointer = document.querySelector("ul");
-
-showTheseVehicles(vehicles);
-
-function showTheseVehicles(arr) {
-  arr.forEach((each) => {
-    ulPointer.innerHTML += `<li>${each.type}</li>`;
-    ulPointer.innerHTML += `<li>${each.fuel}</li>`;
-    ulPointer.innerHTML += `<li>${each.passengers}</li>`;
-    ulPointer.innerHTML += `<li>${each.stops}</li>`;
-    ulPointer.innerHTML += `<li>${each.ownedBy}</li>`;
-    ulPointer.innerHTML += `<li>${each.isElectric}</li>`;
-    ulPointer.innerHTML += `<li>${each.isTandem}</li>`;
-  });
-}
-
-function all(isElectric) {
-  return true;
-  const 
-}
-*/
 const animals = [
   {
     name: "Mandu",
@@ -71,3 +38,40 @@ let onlyCats = animals.filter(isCat);
 console.log("Cats", onlyCats);
 let onlyDogs = animals.filter(isDog);
 console.log("Dogs", onlyDogs);
+*/
+
+const vehicles = [
+  { type: "Bus", fuel: "Diesel", passengers: 45, stops: ["Nørrebrogade", "Elmegade"] },
+  { type: "Bil", fuel: "Benzin", passengers: 4, ownedBy: "Klaus" },
+  { type: "Cykel", fuel: "Rugbrød", passengers: 0, ownedBy: "Jonas", isElectric: true },
+  { type: "Bil", passengers: 5, ownedBy: "Elon", isElectric: true },
+  { type: "MC", fuel: "Benzin", passengers: 2, ownedBy: "Fonda" },
+  { type: "Cykel", fuel: "Rugbrød", passengers: 2, ownedBy: "Vingegård", isTandem: true },
+  { type: "MC", fuel: "Benzin", passengers: 2, ownedBy: "Yolanda" },
+  { type: "Knallert", fuel: "Benzin", passengers: 1, ownedBy: "Børge" },
+  { type: "Knallert", fuel: "Benzin", passengers: 1, ownedBy: "Jonas" },
+  { type: "Løbehjul", passengers: 1, isElectric: true },
+];
+
+const ulPointer = document.querySelector("ul");
+
+showTheseVehicles(vehicles);
+
+function showTheseVehicles(arr) {
+  arr.forEach((each) => {
+    ulPointer.innerHTML += `<li>${each.type}</li>`;
+    ulPointer.innerHTML += `<li>${each.fuel}</li>`;
+    ulPointer.innerHTML += `<li>${each.passengers}</li>`;
+    ulPointer.innerHTML += `<li>${each.stops}</li>`;
+    ulPointer.innerHTML += `<li>${each.ownedBy}</li>`;
+    ulPointer.innerHTML += `<li>${each.isElectric}</li>`;
+    ulPointer.innerHTML += `<li>${each.isTandem}</li>`;
+  });
+}
+
+function isElectric(vehicle) {
+  return vehicle.isElectric === true;
+}
+
+let onlyElectrics = vehicles.filter(isElectric);
+console.log("electrics", onlyElectrics);
