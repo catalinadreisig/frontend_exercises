db245cda6867b37416744762fa1b4b26f99c82ef
catalinadreisig <catalinadreisig@yahoo.dk>
2024-02-07 10:14:49 +0100

    hej

--- breadcrumbs_generator/js.js
+++ breadcrumbs_generator/js.js
@@ -43,22 +43,3 @@ function generateBreadcrumb() {
 
 // Event listener for button click
 document.getElementById("generateBreadcrumb").addEventListener("click", generateBreadcrumb);
-
-const list = document.querySelector("ul");
-
-const li = document.createElement("li");
-const valueArr = [];
-li.style.setProperty("-height", "10");
-list.appendChild(li);
-
-setInterval(genererSoejle, 1000);
-
-function genererSoejle() {
-  valueArr.push(Math.round(Math.random() * 100));
-  console.log(valueArr);
-  const li = document.createElement("li");
-  li.style.setProperty("--height", "10");
-  list.appendChild(li);
-
-  console.log("genererSoejle");
-}
