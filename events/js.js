042c2ebb6b06721c54eb01d89362b0792bfc9c4d
Esther <estherfinsen@gmail.com>
2024-02-13 10:24:49 +0100

    gs

--- events/js.js
+++ events/js.js
@@ -1,5 +1,14 @@
-document.querySelector("button").addEventListener("click),btn_klik");
+document.querySelector("button").addEventListener("click", btn_klik);
 
 function btn_klik(evt) {
-  console.log("evt", evt);
+  document.querySelector("#ball").addEventListener("animationend", animationIterateAndEnd);
+  document.querySelector("#ball").addEventListener("animationiteration", animationIterateAndEnd);
+  document.querySelector("#ball").classList.add("ani");
+}
+
+function animationIterateAndEnd(evt) {
+  console.log("animationIterateAndENd", evt.type);
+  if (evt.type === animationend) {
+    document.querySelector("#ball").classList.remove("ani");
+  }
 }
