4b8b2d38d571ccf32abc2a66483fb374eae8c52e
Esther <estherfinsen@gmail.com>
2024-02-15 12:04:42 +0100

    gs

---
+++ netflix_buttton/js.js
@@ -0,0 +1,10 @@
+/*const nextEpisodeButton = document.getElementById("nextEpisodeButton");
+
+nextEpisodeButton.addEventListener("mouseenter", function () {
+  nextEpisodeButton.classList.add("hovered");
+});
+
+nextEpisodeButton.addEventListener("mouseleave", function () {
+  nextEpisodeButton.classList.remove("hovered");
+});
+*/
